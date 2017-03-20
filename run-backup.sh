6bc8cc2725ba75896ac9fb7a3a4cd0d51cab2033
Jack Murphy <jmurphy.web@gmail.com>
2017-03-20 16:04:50 +0200

    update backup file to backup production db

--- run-backup.sh
+++ run-backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Beginning database backup"
 
 #install aws-cli
 curl https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -o awscli-bundle.zip
@@ -6,8 +7,18 @@ unzip awscli-bundle.zip
 chmod +x ./awscli-bundle/install
 ./awscli-bundle/install -i /tmp/aws
 
-# mysqldump -u root -pcorrect --databases s3_backup_spike | gzip -c > "./tmp/2017-03-20-09-24.sql.gz"
+# new backup file name
+BACKUP_FILE_NAME="$(date +"%Y-%m-%d-%H-%M")-$APP-$DATABASE.sql"
 
-/tmp/aws/bin/aws s3 cp ./2017-03-20-11-24.sql.gz s3://s3-backup-spike/spike/2017-03-20-11-24.sql.gz --region eu-west-1
+# if directory '/tmp/db-backups' doesn't exist, create it
+if [ ! -d "/tmp/db-backups" ]; then
+  mkdir -p /tmp/db-backups/
+fi
 
-echo "backup 2017-03-20-11-24.sql.gz complete"
+# dump the current DB into /tmp/db-backups/<new-file-name>
+mysqldump -u $CLEARDB_USER_NAME -h $CLEARDB_SERVER_IP -p$CLEARDB_PASSWORD --databases $DATABASE | gzip -c > "/tmp/db-backups/$BACKUP_FILE_NAME.gz"
+
+# using the aws cli, copy the new backup to our s3 bucket
+/tmp/aws/bin/aws s3 cp /tmp/db-backups/$BACKUP_FILE_NAME.gz s3://$S3_BUCKET_PATH/$DATABASE/$BACKUP_FILE_NAME.gz --region $AWS_DEFAULT_REGION
+
+echo "backup $BACKUP_FILE_NAME.gz complete"

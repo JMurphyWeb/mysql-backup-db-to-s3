154d8661cac86f13c902a857e43f1720ec2e6d8e
Jack Murphy <jmurphy.web@gmail.com>
2017-03-21 10:20:09 +0200

    fix aws check for file

--- restore-backup.sh
+++ restore-backup.sh
@@ -10,7 +10,7 @@ if [ ! $SHOULD_CONTINUE = y ]; then
 fi
 
 #install aws-cli if it doesn't exist
-if [ ! -d /tmp/aws ]; then
+if [ ! -d "/tmp/aws" ]; then
   curl https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -o awscli-bundle.zip
   unzip awscli-bundle.zip
   chmod +x ./awscli-bundle/install
@@ -19,8 +19,6 @@ fi
 
 # create a backup before attempting to overwrite the current DB
 
-
-# install aws cli if not already installed
 
 
 # create /tmp/s3-backups directory if it doesn't already exist
